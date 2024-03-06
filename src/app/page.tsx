68ac5c737cd6bd766dcd580a0d46980661d397c9
Jonathan Verstraten <Jonathan.verstraten@stater.nl>
2024-03-06 15:32:03 +0100

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import Image from 'next/image';
-
 export default function Home() {
-  return (
-    <main>
-      <div>
-        <p>
-          Get started by editing&nbsp;
-          <code>src/app/page.tsx</code>
-        </p>
-      </div>
-    </main>
-  );
+  return <main>Hier komt de content</main>;
 }
