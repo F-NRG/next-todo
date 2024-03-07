1e11a57f88ab12998f3872860a0a31ec66c6b71f
Jonathan Verstraten <Jonathan.verstraten@stater.nl>
2024-03-07 19:02:40 +0100

    updated config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-require-imports */
+
 const path = require('path');
 const stylexPlugin = require('@stylexjs/nextjs-plugin');
 
