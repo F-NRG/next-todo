b60d96ba2d1d2d61d476ad908a70da8ff5d48373
Jonathan Verstraten <Jonathan.verstraten@stater.nl>
2024-03-13 14:20:28 +0100

    not to include next config in tsconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,13 @@
 // @ts-check
-
 /** @type {import('next').NextConfig} */
 
 import stylexPlugin from '@stylexjs/nextjs-plugin';
-import babelrc from './.babelrc.js';
-
-const __dirname = process.cwd().toString();
-const plugins = babelrc.plugins;
-const stylexBabelPlugin = plugins.find((plugin) => Array.isArray(plugin) && plugin[0] === '@stylexjs/babel-plugin');
-const options = stylexBabelPlugin ? stylexBabelPlugin[1] : undefined;
-
-if (!options) {
-  throw new Error('No options found');
-}
-
-const rootDir = options.unstable_moduleResolution.rootDir ?? __dirname;
-const aliases = options.aliases ?? undefined;
-const useCSSLayers = options.useCSSLayers ?? undefined;
+import stylexBabel from './stylexBabel.js';
 
 const plugin = stylexPlugin({
-  aliases,
-  rootDir,
-  useCSSLayers,
+  aliases: stylexBabel.aliases,
+  rootDir: stylexBabel.rootDir,
+  useCSSLayers: stylexBabel.useCSSLayers,
 })({ transpilePackages: ['@stylexjs/open-props'] });
 
 const nextConfig = { ...plugin };
